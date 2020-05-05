4a365badc980e30aab3e2816b925ef482e6587ab
Reisetter <areisett@uiowa.edu>
2020-05-05 15:59:49 -0500

    incorporating sex checks

--- R/preprocess.R
+++ R/preprocess.R
@@ -3,29 +3,42 @@
 #'
 #' This function allows you to preprocess PLINK bed/bim/fam files for use with \code{penalizedLMM} functions. Unreliable SNPs are removed and missing values are imptued using either \code{snpStats}, or if not tagged, the HWE mean value.
 #' @param prefix Character argument that is the prefix of your bed/bim/fam files.
-#' @param dataDir Directory where plink files are located
+#' @param dataDir Directory where plink files (and .sexcheck files are located if \code{sexcheck = TRUE}) are located
+#' @param sexcheck Logical flag for whether or not PLINK sexcheck files should be incorporated. Defaults to FALSE. If TRUE, sexcheck files must be of the form "prefix.sexcheck"
+#' @param na.strings For \code{snpStats}. Strings in .bam and .fam files to be recoded as NA. Defaults to "-9"
 #' @return A three element list object:
 #' * `genotypes` The filtered and imputed genotypes in a snpMatrix object with subjects in rows and SNPs in columns.
 #' * `map` A matrix of SNP data.
 #' * `fam` A matrix of subject data.
 #' @export
 
-preprocess <- function(prefix, dataDir){
+preprocess <- function(prefix, dataDir, sexcheck = FALSE, na.strings = "-9"){
 
   cat("\nPreprocessing", prefix, "data:\n")
 
-  obj <- snpStats::read.plink(file.path(dataDir, prefix), na.strings = "-9") # `na.strings = "-9"` makes chr and position show up as 0 instead of NA in the map obj
+  obj <- snpStats::read.plink(file.path(dataDir, prefix), na.strings = na.strings)
 
   # Only keep polygenic SNPs and those on chr1:22
   genotypes <- obj$genotypes[, snpStats::col.summary(obj$genotypes)$P.AA != 1 &
                                snpStats::col.summary(obj$genotypes)$P.AB != 1 &
                                snpStats::col.summary(obj$genotypes)$P.BB != 1 & obj$map$chromosome %in% 1:22]
 
-  cat("\nRemoving ", dim(obj$geno)[2] - dim(genotypes)[2], "SNPs that are not polygenic or outside of chr 1:22\n")
+  cat("\nRemoving ", dim(obj$geno)[2] - dim(genotypes)[2], "SNPS that are monomorphic or outside of chr 1:22\n")
+
+  # if sexcheck = TRUE, remove subjects with sex discrepancies
+  if (sexcheck == TRUE) {
+    sexdat <- data.table::fread(file.path(dataDir, paste0(prefix, ".sexcheck")))
+    iid_sex_discrep <- as.character(sexdat[sexdat$STATUS == "PROBLEM", "IID"])
+    if (length(iid_sex_discrep) > 0){
+      genotypes <- genotypes[-match(iid_sex_discrep, rownames(genotypes)), ]
+    }
+    cat("\nRemoving", ifelse(length(iid_sex_discrep) > 0, length(iid_sex_discrep), 0), "subjects with sex discrepancies\n")
+
+  }
 
   # Now how many SNPs have missing data?
   missing <- table(snpStats::col.summary(genotypes)$Call.rate == 1)
-  cat("\n", ifelse(length(missing) == 2, missing[1], 0), "SNPs with missing data that we will attempt to impute\n")
+  cat("\n", ifelse(length(missing) == 2, missing[1], 0), "SNPS with missing data that we will attempt to impute\n")
 
   # Impute missing values
   rules <- snpStats::snp.imputation(genotypes, minA=0)
@@ -33,7 +46,7 @@ preprocess <- function(prefix, dataDir){
 
   # How many SNPs have missing data after imputation?
   missing <- table(snpStats::col.summary(out)$Call.rate == 1)
-  cat("\n", ifelse(length(missing) == 2, missing[1], 0), "SNPs with missing data after imputation\n")
+  cat("\n", ifelse(length(missing) == 2, missing[1], 0), "SNPS with missing data after imputation\n")
 
   # How many SNPs cannot be imputed and still have >= 50% missing values?
   missing <- table(snpStats::col.summary(out)$Call.rate <= 0.5)
@@ -44,7 +57,7 @@ preprocess <- function(prefix, dataDir){
 
   # How many SNPs will be imputed with mean values?
   missing <- table(snpStats::col.summary(out2)$Call.rate == 1)
-  cat("\n", ifelse(length(missing) == 2, missing[1], 0), "SNPs still have missing data that will be imputed with the HWE expected value\n")
+  cat("\n", ifelse(length(missing) == 2, missing[1], 0), "SNPS still have missing data that will be imputed with the HWE expected value\n")
 
   # keep snp order for later
   order_snps <- colnames(out2)
@@ -64,14 +77,14 @@ preprocess <- function(prefix, dataDir){
   out3@.Data[, to_impute] <- imputed_mean
 
   # make sure there are no missing values remaining
-  missing <- 0
-  chunks <- ceiling(nrow(out3) / 100)
-  start <- 1
-  for (i in 1:chunks){
-    stop <- min(i*100, nrow(out3))
-    missing <- missing + sum(is.na(out3[start:stop]))
-    start <- stop + 1
-  }
+  # missing <- 0
+  # chunks <- ceiling(nrow(out3) / 100)
+  # start <- 1
+  # for (i in 1:chunks){
+  #   stop <- min(i*100, nrow(out3))
+  #   missing <- missing + sum(is.na(out3[start:stop]))
+  #   start <- stop + 1
+  # }
 
   # keep corresponding map data
   map <- obj$map[colnames(out3),]
