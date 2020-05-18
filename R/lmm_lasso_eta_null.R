ddd764a6eddb36837ece55d5e0701b889116c214
Reisetter <areisett@uiowa.edu>
2020-05-18 16:52:01 -0500

    replacing svd(K) with eigen(K) for convergence stability

--- R/lmm_lasso_eta_null.R
+++ R/lmm_lasso_eta_null.R
@@ -9,7 +9,7 @@
 lmm_lasso_eta_null<-function(X, y){
   S <- U <- V <- NULL
   K <- tcrossprod(ncvreg::std(X))/ncol(X)
-  c(S, U, V) %<-% svd(K)
+  c(S, U) %<-% methods::as(eigen(K), "list")
   Uy <- crossprod(U, y)
   opt <- stats::optimize(f=lmm_lasso_eta_nll, c(0.01, 0.99), Uy=Uy, S=S)
   eta <- opt$minimum
