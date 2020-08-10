6fc7f3e1619b0018d7e04e0a06f3538d30661580
Reisetter <areisett@uiowa.edu>
2020-08-10 13:38:40 -0500

    test_plmm.R: adding tests for unpenalized covariates

--- inst/tinytest/test_plmm.R
+++ inst/tinytest/test_plmm.R
@@ -4,13 +4,14 @@
 # X <- Data$X
 # y <- Data$y
 
-nn <- 5
+nn <- 10
 pp <- 2
 p1 <- 1
-
+set.seed(7)
 X <- matrix(rnorm(nn * pp), nrow = nn, ncol = pp)
+X0 <- matrix(c(rnorm(nn * 1)), nrow = nn, ncol = 1)
 B <- rep(c(1, 0), times = c(p1, pp - p1))
-y <- X %*% B + rnorm(nn)
+y <- X %*% B + X0 %*% c(1) + rnorm(nn)
 
 dont_run <- FALSE
 
@@ -56,13 +57,63 @@ glm2 <- glmnet::glmnet(X, y, "gaussian", standardize = TRUE, lambda = plmm2$lamb
 expect_equivalent(plmm2$beta, coef(ncv2), tol = 1e-3)
 expect_equivalent(plmm2$beta, as.matrix(coef(glm2)), tol = 1e-3)
 
-### add check for unpenalized covars here with additional equiv.check - modify penalty.factor for glmnet
+### no rotation + unpenalized covar checks-----------------------------------###
+
+### these are not passing test...pass at 1e-1 (?)
+
+# std(X)
+plmm3 <- plmm(ncvreg::std(cbind(X0, X)),
+              y,
+              X_for_K = ncvreg::std(X),
+              penalty = "lasso",
+              penalty.factor = rep(c(0, 1), times = c(ncol(X0), ncol(X))),
+              alpha = 1,
+              nlambda = 5,
+              eps = 1e-12,
+              max.iter = 1000,
+              convex = FALSE,
+              warn = TRUE,
+              standardize = FALSE,
+              rotation = FALSE,
+              returnX = FALSE)
+
+glm3 <- glmnet::glmnet(ncvreg::std(cbind(X0, X)), y, "gaussian",
+                       standardize = FALSE, lambda = plmm3$lambda,
+                       penalty.factor = rep(c(0, 1), times = c(ncol(X0), ncol(X))))
+
+expect_equivalent(plmm3$beta, as.matrix(coef(glm3)), tol = 1e-3)
+expect_equivalent(plmm3$beta, as.matrix(coef(glm3)), tol = 1e-1)
+
+
+# X
+plmm4 <- plmm(cbind(X0, X),
+              y,
+              X_for_K = X,
+              penalty = "lasso",
+              penalty.factor = rep(c(0, 1), times = c(ncol(X0), ncol(X))),
+              alpha = 1,
+              nlambda = 5,
+              eps = 1e-04,
+              max.iter = 1000,
+              convex = FALSE,
+              warn = TRUE,
+              standardize = TRUE,
+              rotation = FALSE,
+              returnX = FALSE)
+
+glm4 <- glmnet::glmnet(cbind(X0, X), y, "gaussian",
+                       standardize = TRUE, lambda = plmm4$lambda,
+                       penalty.factor = rep(c(0, 1), times = c(ncol(X0), ncol(X))))
+
+expect_equivalent(plmm4$beta, as.matrix(coef(glm4)), tol = 1e-3)
+expect_equivalent(plmm4$beta, as.matrix(coef(glm4)), tol = 1e-1)
 
 
 ### rotation checks ---------------------------------------------------------###
+### compare with ols solutions
 
 # std(X)
-plmm3 <- plmm(ncvreg::std(X),
+plmm5 <- plmm(ncvreg::std(X),
               y,
               penalty = "lasso",
               alpha = 1,
@@ -76,12 +127,12 @@ plmm3 <- plmm(ncvreg::std(X),
               returnX = TRUE)
 
 if (nrow(X) > ncol(X)){
-  ols_soln <- solve(t(plmm3$X) %*% plmm3$X) %*% t(plmm3$X) %*% plmm3$y
-  expect_equivalent(plmm3$beta, ols_soln, tol = 1e-3)
+  ols_soln <- solve(t(plmm5$X) %*% plmm5$X) %*% t(plmm5$X) %*% plmm5$y
+  expect_equivalent(plmm5$beta, ols_soln, tol = 1e-3)
 }
 
 # X
-plmm4 <- plmm(X,
+plmm6 <- plmm(X,
               y,
               penalty = "lasso",
               alpha = 1,
@@ -96,8 +147,53 @@ plmm4 <- plmm(X,
 
 if (nrow(X) > ncol(X)){
   ols_soln <- lm(y ~ X)
-  expect_equivalent(as.numeric(plmm4$beta), coef(ols_soln), tol = 1e-3)
+  expect_equivalent(as.numeric(plmm6$beta), coef(ols_soln), tol = 1e-3)
 }
+
+### rotation + unpenalized covar checks-----------------------------------###
+
+# std(X)
+plmm7 <- plmm(ncvreg::std(cbind(X0, X)),
+              y,
+              penalty = "lasso",
+              alpha = 1,
+              lambda = 0, # compare to ols solutions
+              eps = 1e-4,
+              max.iter = 1e3,
+              convex = FALSE,
+              warn = TRUE,
+              standardize = FALSE,
+              rotation = TRUE,
+              returnX = TRUE)
+
+if (nrow(cbind(X0, X)) > ncol(cbind(X0, X))){
+  ols_soln <- solve(t(plmm7$X) %*% plmm7$X) %*% t(plmm7$X) %*% plmm7$y
+  expect_equivalent(plmm7$beta, ols_soln, tol = 1e-3)
+}
+
+# X
+plmm8 <- plmm(cbind(X0, X),
+              y,
+              penalty = "lasso",
+              alpha = 1,
+              lambda = 0, # compare to ols solutions
+              eps = 1e-4,
+              max.iter = 1e3,
+              convex = FALSE,
+              warn = TRUE,
+              standardize = FALSE,
+              rotation = TRUE,
+              returnX = TRUE)
+
+if (nrow(cbind(X0, X)) > ncol(cbind(X0, X))){
+  ols_soln <- lm(y ~ cbind(X0, X))
+  expect_equivalent(as.numeric(plmm8$beta), coef(ols_soln), tol = 1e-3)
+}
+
+
+
+
+
 
 ### try comparing to glmnet - to plmm, relax tol
 
@@ -129,3 +225,5 @@ if (dont_run){
 ### for the unrotated version of X, glmnet would give the standardization for the
 ### rotated version, which will not be the same.
 ### Using the unrotated test to check that standardization is working as it should be
+
+
