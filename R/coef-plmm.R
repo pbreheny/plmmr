a73b39809669bddd24caa8987a146a1db58305a0
Reisetter <areisett@uiowa.edu>
2020-08-13 17:31:00 -0500

    adding s3 methods and helper fun

---
+++ R/coef-plmm.R
@@ -0,0 +1,22 @@
+# from ncvreg
+
+coef.plmm <- function (object, lambda, which = 1:length(object$lambda), drop = TRUE,  ...){
+  if (!missing(lambda)) {
+    if (max(lambda) > max(object$lambda) | min(lambda) <
+        min(object$lambda)) {
+      stop("Supplied lambda value(s) are outside the range of the model fit.",
+           call. = FALSE)
+    }
+    ind <- approx(object$lambda, seq(object$lambda), lambda)$y
+    l <- floor(ind)
+    r <- ceiling(ind)
+    w <- ind%%1
+    beta <- (1 - w) * object$beta[, l, drop = FALSE] + w *
+      object$beta[, r, drop = FALSE]
+    colnames(beta) <- lamNames(lambda)
+  }
+  else beta <- object$beta[, which, drop = FALSE]
+  if (drop)
+    return(drop(beta))
+  else return(beta)
+}
