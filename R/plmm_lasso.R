2362b87494bd361346c1f48e6127d742e62051a9
areisett <13437042+areisett@users.noreply.github.com>
2021-05-02 12:55:11 -0500

    updating plmm_lasso wrapper fun

--- R/plmm_lasso.R
+++ R/plmm_lasso.R
@@ -4,19 +4,18 @@
 #' @param X Design matrix.
 #' @param y Continuous outcome vector.
 #' @param p1 Number of causal SNPs. Lambda will be selected such that <= p1 variables enter the model.
-#' @param standardize Should standardization be performed within \code{glmnet()}? Defaults to FALSE.
-#' @param X_for_K X matrix used to compute the similarity matrix, K. For multi-chromosome analysis this may be supplied in order to perform a leave-one-chromosome-out correction. The objective here is to adjust for population stratification and unobserved confounding without rotating out the causal SNP effects.
+#' @param ... Additional optional arguments
 #' @importFrom zeallot %<-%
 #' @export
 
 
 
-plmm_lasso <- function(X, y, p1, standardize = FALSE, X_for_K = NULL) {
-  if (is.null(X_for_K)){
-    fit <- plmm(X, y, X, penalty = 'lasso', standardizeX = standardize, standardizeRtX = FALSE)
-  } else {
-    fit <- plmm(X, y, X_for_K, penalty = 'lasso', standardizeX = standardize, standardizeRtX = FALSE)
-  }
+plmm_lasso <- function(X, y, p1, ...) {
+  args.list <- list(...)
+  args.list$X <- X
+  args.list$y <- y
+  args.list$penalty <- 'lasso'
+  fit <- do.call('plmm', args.list)
   sel <- predict.plmm(fit, type = "nvar")
   coef <- coef(fit, min(fit$lambda[sel <= p1]))[-1]
   coef_pred <- coef(fit, min(fit$lambda[sel <= p1]))
