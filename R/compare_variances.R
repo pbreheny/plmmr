6b3fd1738194d11f265809c1466a486c4c582e43
Tabitha Peter <tabitha-peter@uiowa.edu>
2024-03-12 14:48:30 -0500

    document new funtions

--- R/compare_variances.R
+++ R/compare_variances.R
@@ -1,18 +1,17 @@
 #' quick function to do some further simulations 
 #'
-#' @param nrep # of reps for simulation
-#' @param K 
-#' @param sig_s 
-#' @param sig_eps 
-#' @param intercept 
-#' @param center_y 
+#' @param nrep Number of reps for simulation
+#' @param K Realized relatedness matrix 
+#' @param sig_s variance due to structure
+#' @param sig_eps variance due to noise
+#' @param intercept Logical: defaults to TRUE.
+#' @param center_y Logical: defaults to FALSE.
 #'
 #' @return list with two named vectors: 
 #'  * intercept: estimated eta values from the method that defines null model as intercept-only model
 #'  * zero_mean: estimated eta values from the model that defines y to have mean 0 on rotated scale. 
 #' @export
 #'
-#' @examples
 compare_variances <- function(nrep, K, sig_s, sig_eps,
                               intercept = TRUE, center_y = FALSE){
   hat_eta <- lippert_hat_eta <- rep(NA_integer_, nrep)
