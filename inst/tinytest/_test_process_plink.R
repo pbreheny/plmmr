d1120807f5648514011babaaddbfa275cd5f0a07
Tabitha Peter <tabitha-peter@uiowa.edu>
2024-07-13 10:45:44 -0500

    process_plink() and create_design() are now passing all tests needed to handle my dissertation data

--- inst/tinytest/_test_process_plink.R
+++ inst/tinytest/_test_process_plink.R
@@ -34,7 +34,6 @@ if (interactive()){
 
   phen <- cbind(penncath_pheno$FamID, penncath_pheno$CAD) |>
     as.data.frame() |>
-    tidyr::drop_na() |>
     as.matrix()
   colnames(phen) <- c("FamID", "CAD") # CAD has no missing values in phen file
 
@@ -46,7 +45,6 @@ if (interactive()){
                      add_phen = phen,
                      pheno_id = "FamID",
                      pheno_name = "CAD",
-                     na_phenotype_vals = c(NA_integer_),
                      add_predictor_ext = predictors,
                      id_var = "FID",
                      overwrite = TRUE,
@@ -88,7 +86,6 @@ rownames(predictors) <- penncath_pheno$FamID
 
 phen <- cbind(penncath_pheno$FamID, penncath_pheno$CAD) |>
   as.data.frame() |>
-  tidyr::drop_na() |>
   as.matrix()
 colnames(phen) <- c("FamID", "CAD") # CAD has no missing values in phen file
 
@@ -100,7 +97,6 @@ X <- create_design(dat = penncath_lite,
                    add_phen = phen,
                    pheno_id = "FamID",
                    pheno_name = "CAD",
-                   na_phenotype_vals = c(NA_integer_),
                    add_predictor_ext = predictors,
                    id_var = "FID",
                    overwrite = TRUE,
@@ -147,7 +143,6 @@ phen <- cbind(penncath_pheno$FamID, penncath_pheno$hdl) |>
 colnames(phen) <- c("FamID", "hdl")
 
 ### create design --------------------------------------------------------------
-
 X <- create_design(dat = penncath_lite,
                    rds_dir = "inst/extdata",
                    prefix = "std_penncath_lite",
@@ -184,19 +179,64 @@ list.files("inst/extdata", pattern = "*.bk")
 # clear example
 rm(X); rm(res); rm(predictors); rm(phen)
 
-# test 4: no external files supplied -------------------------------------------
+# test 4: more obs. in pheno than geno ------------------------------------------
+penncath_pheno <- read.csv("inst/extdata/penncath_clinical.csv")
+
+# subset geno data
+n1000 <- sample(1:nrow(imputed_dat$genotypes), 1000) |> sort()
+geno_n1000 <- snp_subset(x = imputed_dat,
+                         ind.row = n1000)
+
+bigsnp_n1000 <- snp_attach(geno_n1000)
+bigsnp_n1000$colnames <- imputed_dat$colnames
+bigsnp_n1000$rownames <- imputed_dat$rownames[n1000]
+bigsnp_n1000$n <- 1000
+bigsnp_n1000$p <- ncol(bigsnp_n1000$genotypes)
+snp_save(bigsnp_n1000)
+
+predictors <- penncath_pheno |>
+  dplyr::select(FamID, age, tg) |>
+  dplyr::mutate(tg = dplyr::if_else(is.na(tg), mean(tg, na.rm = T), tg)) |>
+  tibble::column_to_rownames('FamID') |>
+  as.matrix()
+colnames(predictors) <- c("age", "tg")
+
+phen <- cbind(penncath_pheno$FamID, penncath_pheno$CAD) |>
+  as.data.frame() |>
+  as.matrix()
+colnames(phen) <- c("FamID", "CAD") # CAD has no missing values in phen file
 
 ### create design ---------------------------------------------------------------
+X <- create_design(dat = geno_n1000,
+                   rds_dir = 'inst/extdata',
+                   prefix = "std_penncath_n1000",
+                   is_bigsnp = T,
+                   add_phen = phen,
+                   pheno_id = "FamID",
+                   pheno_name = "CAD",
+                   add_predictor_ext = predictors,
+                   id_var = "FID",
+                   overwrite = TRUE,
+                   outfile = "design_penncath_n1000")
 
+res <- readRDS(X)
+str(res)
 
 ### checks  -------------------------------------------------------------------
 # are external files and PLINK fam file aligned on ID var?
+tinytest::expect_identical(as.character(res$fam$family.ID), res$X_rownames)
 
 # does final fam[,6] have the expected outcome?
+table(res$fam[,6], penncath_pheno$CAD[n1000], useNA = 'ifany')
 
 # are row & column names (IDs) aligned?
+str(res$X_colnames); str(res$X_rownames)
+str(res$std_X_colnames); str(res$std_X_rownames)
+tinytest::expect_identical(res$std_X_rownames, res$X_rownames[res$complete_phen])
+tinytest::expect_identical(res$std_X_colnames, res$X_colnames[res$ns])
 
 # are .bk files 'cleaned up' and labeled correctly?
+list.files("inst/extdata", pattern = "*.bk")
 
 # clear example
 rm(X); rm(res); rm(predictors); rm(phen)
